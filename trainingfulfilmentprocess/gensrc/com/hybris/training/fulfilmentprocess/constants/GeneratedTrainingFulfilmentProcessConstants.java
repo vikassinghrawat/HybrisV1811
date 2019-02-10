8fd8a1e8dc4ac71afb18f66edacb7bada01b22b2
vikasrawat <vikass@pragiti.com>
2019-02-10 16:56:52 +0530

    My first commit

---
+++ trainingfulfilmentprocess/gensrc/com/hybris/training/fulfilmentprocess/constants/GeneratedTrainingFulfilmentProcessConstants.java
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 4, 2019 11:24:49 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "trainingfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedTrainingFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ traininginitialdata/gensrc/com/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 4, 2019 11:24:49 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
